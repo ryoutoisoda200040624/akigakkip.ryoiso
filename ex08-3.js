db5009a7705f003b1c5707dc89c65a47052ca29b
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-22 11:55:47 +0900

    Update ex08-3.js

--- ex08-3.js
+++ ex08-3.js
@@ -5,6 +5,12 @@ function kotae1(){
   }else if(document.getElementById('answer1').value=='あお')
            {
     alert('正解！');
+           } else if(document.getElementById('answer1').value=='青色')
+           {
+    alert('正解！');  
+           } else if(document.getElementById('answer1').value=='あおいろ')
+           {
+    alert('正解！');  
   }else{
     alert('残念。正解は青！')
   }
