73a38a70eb7ee2814c02dc6d8128df2bff5b2e8d
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-22 12:04:32 +0900

    Update ex08-5.js

--- ex08-5.js
+++ ex08-5.js
@@ -1,1 +1,17 @@
-
+function kotae3(){
+  if(document.getElementById('answer3').value=='赤')
+  {
+    alert('正解！');
+  }else if(document.getElementById('answer3').value=='あか')
+           {
+    alert('正解！');
+           } else if(document.getElementById('answer3').value=='赤色')
+           {
+    alert('正解！');  
+           } else if(document.getElementById('answer3').value=='あかいろ')
+           {
+    alert('正解！');  
+  }else{
+    alert('残念。正解は赤！')
+  }
+}
