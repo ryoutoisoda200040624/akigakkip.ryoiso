ddf53183d62f122992bd5c54f1edecf715b0e054
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-22 12:00:22 +0900

    Update ex08-4.js

--- ex08-4.js
+++ ex08-4.js
@@ -1,1 +1,11 @@
-
+function kotae2(){
+  if(document.getElementById('answer2').value=='ヨッシー')
+  {
+    alert('正解！');
+  }else if(document.getElementById('answer2').value=='よっしー')
+           {
+    alert('正解！');
+           } else{
+    alert('残念。正解はヨッシー！')
+  }
+}
