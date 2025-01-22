b1d277cdf356c3d6a3a165d1e2ef7c0818df88b7
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-22 11:18:23 +0900

    Update ex08-1.js

--- ex08-1.js
+++ ex08-1.js
@@ -1,2 +1,5 @@
 function imgchange1(){document.getElementById('mario').src='./image/neko.png';}
 function imgchange2(){document.getElementById('mario').src='./image/mariohutuu.png';}
+function imgchange3(){document.getElementById('mario').src='./image/.png';}
+function imgchange4(){document.getElementById('mario').src='./image/aisu.png';}
+function imgchange5(){document.getElementById('mario').src='./image/penginn.png';}
