af9be58f1665be6c78ae7e3f3a3e9d295c3e6f0c
ryoutoisoda200040624 <168411148+ryoutoisoda200040624@users.noreply.github.com>
2025-01-22 11:52:56 +0900

    Update ex08-3.js

--- ex08-3.js
+++ ex08-3.js
@@ -1,1 +1,11 @@
-
+function kotae1(){
+  if(document.getElementById('answer1').value=='青')
+  {
+    alert('あたり！');
+  }else if(document.getElementById('answer1').value=='あお')
+           {
+    alert('あたり！');
+  }else{
+    alert('はずれ！')
+  }
+}
